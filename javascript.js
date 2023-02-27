e2720739550df7c7913feebaacf30abd2c96feed
nicopriotto <nico.priotto@gmail.com>
2023-02-27 01:22:39 -0300

    Toggle not matching pswd message

--- javascript.js
+++ javascript.js
@@ -1,14 +1,16 @@
 const password = document.getElementById("password");
 const confirm_password = document.getElementById("confirm_password");
+const errorMessage = document.querySelector(".error_message");
 
 function checkPwd() {
     if (password.value === confirm_password.value) {
         password.classList.remove('error');
         confirm_password.classList.remove('error');
-        console.log("sii")
+        errorMessage.style.display = "none";
     } else {
         password.classList.add('error');
         confirm_password.classList.add('error');
+        errorMessage.style.display = "block";
     }
 }
 
